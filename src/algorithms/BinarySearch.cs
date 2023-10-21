2f472c98362487ea156b17fb67778ced3a548158
Mikhail Bereberdin <bereberdin999@icloud.com>
2023-10-21 14:03:42 +0700

    Реализация бинарного поиска

---
+++ src/algorithms/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿namespace algorithms;
+
+/// <summary>
+/// Бинарный поиск.
+/// </summary>
+public static class BinarySearch
+{
+    /// <summary>
+    /// Попытаться найти индекс элемента.
+    /// </summary>
+    /// <param name="sortedData">Отсортированная коллекция для поиска.</param>
+    /// <param name="elementToFind">Элемент, индекс которого необходимо найти.</param>
+    /// <param name="elementIndex">Индекс найденного элемента. Если элемент не найден - -1.</param>
+    /// <returns>True - если удалось найти индекс элемента, иначе - false.</returns>
+    public static bool TryFindElementIndex(int[] sortedData, int elementToFind, out int elementIndex)
+    {
+        elementIndex = -1;
+
+        var leftLimit = 0;
+        var rightLimit = sortedData.Length - 1;
+
+        while (leftLimit != rightLimit)
+        {
+            var middleIndex = (leftLimit + rightLimit) / 2;
+            var middleElement = sortedData[middleIndex];
+
+            if (middleElement == elementToFind)
+            {
+                elementIndex = middleIndex;
+
+                return true;
+            }
+
+            if (middleElement > elementToFind)
+            {
+                rightLimit = middleIndex - 1;
+            }
+            else
+            {
+                leftLimit = middleIndex + 1;
+            }
+        }
+
+        if (sortedData[leftLimit] == elementToFind)
+        {
+            elementIndex = leftLimit;
+
+            return true;
+        }
+
+        return false;
+    }
+}
